a5debb5b15a61a06fda576b14e1716b492a5f7b0
Josefino Regalario <65765342+quatre044@users.noreply.github.com>
2025-08-16 14:44:38 +0800

    Fix router and entry point to render landing page correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-// Main App component
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Product from './pages/Product';
-import AdminDashboard from './admin/AdminDashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/about' component={About} />
-                <Route path='/services' component={Services} />
-                <Route path='/contact' component={Contact} />
-                <Route path='/product' component={Product} />
-                <Route path='/admin' component={AdminDashboard} />
-            </Switch>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add other routes here as needed */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
